338d4e4c3199144bdaf43b638305b13e9f0d5b71
zluo <zhanqing_luo@silanis.com>
2016-02-02 16:18:05 -0500

    first plot

--- main/mylib/plots.py
+++ main/mylib/plots.py
@@ -3,5 +3,32 @@ Created on Feb 2, 2016
 
 @author: zluo
 '''
-def plot_features():
+import matplotlib.pyplot as plt
+'''
+  m is a nynum array[[],[],[]]
+'''
+def plot_features(m): 
     print('-----------print features----------')
+    len = m.shape[0]
+    i = 0
+    while i < len :
+       li = premature(i, len, m)
+       plot_scatter(li)
+       i += 1
+            
+def premature(i, len, m): 
+    li =[]
+    j = i + 1
+    while(j < len):
+        li.append((m[i],m[j]))
+        j += 1
+    return li
+
+def plot_scatter(li):
+        for tup in li:
+            plt.scatter(tup[0], tup[1])
+            plt.show()
+        
+        
+           
+    
--- test/test_mylib.py
+++ test/test_mylib.py
@@ -1,8 +1,24 @@
 '''
 Created on Feb 2, 2016
 
+python -m unittest test_module1 test_module2
+
+need to understand py deploy
+
+
 @author: zluo
 '''
+import unittest
+import numpy as np
 from mylib.plots import plot_features as pf
- 
-pf()
+
+class TestPlot(unittest.TestCase): 
+    
+    def test_pf(self):
+        a = np.random.rand(100)
+        b = np.sqrt(1 - a*a)  # a*a + b*b =1
+        c = np.vstack((a, b))
+        pf(c)
+
+if __name__ == '__main__':
+    unittest.main()
