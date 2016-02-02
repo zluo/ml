b3582987e851e06929e5b941bc5c187590af116e
zluo <zhanqing_luo@silanis.com>
2016-02-02 11:17:52 -0500

    #package working

---
+++ main/mylib/plots.py
@@ -0,0 +1,7 @@
+'''
+Created on Feb 2, 2016
+
+@author: zluo
+'''
+def plot_features():
+    print('-----------print features----------')
---
+++ test/test_mylib.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 2, 2016
+
+@author: zluo
+'''
+from mylib.plots import plot_features as pf
+ 
+pf()
