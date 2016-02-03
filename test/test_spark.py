092fab0602fbd1b8735c7b7b99165baf63b7a2be
zluo <zhanqing_luo@silanis.com>
2016-02-03 16:44:01 -0500

    work on spark

--- test/test_spark.py
+++ test/test_spark.py
@@ -5,10 +5,21 @@ Created on Feb 3, 2016
 '''
 from pyspark import SparkConf,SparkContext
 import os
+import numpy as np
 
 sparkConf = SparkConf().setAppName('WordCounts').setMaster('local')
 sc = SparkContext(conf = sparkConf)
 
-textFile =sc.parallelize([1,2], 2)
 
-print (textFile.take(2))
+a = np.arange(0, 1, 0.01)
+b = np.sqrt(1 - a*a)  # a*a + b*b =1
+c = a*a  # a*a + b*b =1
+d = a*b
+e =d*c
+f=2*e
+g= a + b + c + d + e + f
+z = np.vstack((a, b, c, d, e, f, g))
+        
+rdd =sc.parallelize(z.T, 2)
+
+print(rdd.take(2))
